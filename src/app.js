4bb0105c0b3831ba99ee28a1d7966ce811b3f7c7
Abdullah Shahid <urfvabdullah@gmail.com>
2026-04-04 14:34:35 +0500

    fix: add dashboard URL to CORS

--- src/app.js
+++ src/app.js
@@ -18,7 +18,13 @@ const logger           = require('./utils/logger');
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://whatsapp-automation-1-sve6.onrender.com',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined', {
